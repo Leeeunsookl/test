4f06d73547e12fc0ce74eed09856b0e48286c671
eunsook <dmstnr738@gmail.com>
2024-01-31 14:30:51 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+    //수업목표 오늘의 수업 목표입니다.
+
+    //여기는 주석입니다.
+    /*여기는
+    * 여러줄
+    * 주석 입니다.
+    * */
+
+    /*
+    *  * ctrl-space:자동완성 기능
+    * ctrl-d
+    * */
+
+    // () {} []
+    // /* */
+    // 복사 예시
+    // 복사 예시
+
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다." + "김은 맛있다."+'강'+120/12+'이'+'은');
+    }
+
+
+}
+
+
